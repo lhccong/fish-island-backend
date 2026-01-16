a3ef7e02f70351b7b3fb15364505aaddd1e29875
liuhuaicong <liuhuaicong@ijianxin.com>
2026-01-16 20:03:42 +0800

    fix(to): 添加对热榜数据为空的处理

--- src/main/java/com/cong/fishisland/job/cycle/IncSyncHostPostToMySQL.java
+++ src/main/java/com/cong/fishisland/job/cycle/IncSyncHostPostToMySQL.java
@@ -16,6 +16,7 @@ import org.springframework.util.StopWatch;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * 自动同步热榜数据
@@ -73,6 +74,11 @@ public class IncSyncHostPostToMySQL {
             }
         }
         HotPost hotPost = dataSourceRegistry.getDataSourceByType(key).getHotPost();
+
+        if (hotPost == null|| Objects.equals(hotPost.getHostJson(), "[]")) {
+            return;
+        }
+        
         hotPost.setType(key);
         if (oldHotPost != null) {
             hotPost.setId(oldHotPost.getId());
